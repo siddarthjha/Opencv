f496133ebd91efa1e15460751467289a2fde6c6e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-06 08:33:11 +0530

    Update Video.py

--- GUI Features/Video.py
+++ GUI Features/Video.py
@@ -35,4 +35,27 @@ while cap.isOpened():
 		break
 cap.release()
 cv2.destroyAllWindows()
-	
+
+# TO SAVE/WRITE THE VIDEO
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)     
+if cap.isOpened() == False:
+	print('Error')
+	cap.open()
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')                      # DIVX IS FOR WINDOWS
+# * FOURCC = Four character code - is a sequence of bytes used to uniquely identify data formats
+
+width = int(cap.get(3))                                                      # It returns the width
+height = int(cap.get(4))                                                     # It returns the height   Default value = (640*480) width*height
+
+out = cv2.VideoWriter('video_write.mkv', fourcc, 20, (width, height))        # This function (parameter-Video Name, fourcc code, frames per sec, (width and height))     
+
+while cap.isOpened():
+	ret, frame = cap.read()      
+	out.write(frame)   
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(10) & 0xFF == 27:
+		break
+cap.release()       
+out.release()               
+cv2.destroyAllWindows()
+
