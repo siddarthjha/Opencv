dd5cfbc0626666939d85104b748328586a4b7f09
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-06 07:57:57 +0530

    Update Video.py

--- GUI Features/Video.py
+++ GUI Features/Video.py
@@ -3,13 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture(0)                                     # This function enables the camera and capture the video 
-
+# if using third party camera then use 1 instead of 0
 
 if cap.isOpened() == False:
 	print('Error')
+	cap.open()                                            # To open the camera if not opened
 
 while cap.isOpened():
-	 # Capture frame-by-frame
+	# Capture frame-by-frame
 	ret, frame = cap.read()                               # This function reads the captured video frame by frame and returns boolean value
 	
 	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)       # This function converts the color video into gray
