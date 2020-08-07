1c249a896b818404ebec86b99047a9c3629225f2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-07 11:38:10 +0530

    Update Paintbrush.py

--- GUI Features/Paintbrush.py
+++ GUI Features/Paintbrush.py
@@ -25,29 +25,29 @@ cv2.destroyAllWindows()
 # MORE ADVANCED DEMO
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
+ix, iy = -1, -1
 
 # mouse callback function
-def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+def draw_circle(event, x, y, flags, param):
+    global ix, iy, drawing, mode
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
-        ix,iy = x,y
+        ix, iy = x, y
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
             if mode == True:
-                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+                cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
             else:
-                cv2.circle(img,(x,y),5,(0,0,255),-1)
+                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if mode == True:
-            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
         else:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
+            cv2.circle(img, x, y), 5, (0, 0, 255), -1)
 
 
 img = np.zeros((512, 512, 3), np.uint8)
