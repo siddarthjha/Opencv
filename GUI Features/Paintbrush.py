a7831120ed10e46f158049bcb4ffae083836c284
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-07 11:31:14 +0530

    Update Paintbrush.py

--- GUI Features/Paintbrush.py
+++ GUI Features/Paintbrush.py
@@ -50,3 +50,17 @@ def draw_circle(event,x,y,flags,param):
             cv2.circle(img,(x,y),5,(0,0,255),-1)
 
 
+img = np.zeros((512, 512, 3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_circle)
+
+while(1):
+    cv2.imshow('image', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
+
+cv2.destroyAllWindows()            
+
