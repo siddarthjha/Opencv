110c3d9a158e9a9985e530d2ece11e2888f55bb8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 12:03:39 +0530

    Update ImageOperations.py

--- Core Operations/ImageOperations.py
+++ Core Operations/ImageOperations.py
@@ -10,5 +10,9 @@ print(green)
 print(red)
 all = img[3000, 120]   # If you cross the dimensions of the image then you will get an index error-(try [7000,10000])
 print(all)
+# You can also change the value
+img[100, 100] = [255, 0, 0]
+print(img[100, 100)
+# Numpy is a optimized library for fast array calculations. So simply accessing each and every pixel values and modifying it will be very slow and it is discouraged.         
 cv2.waitKey(0)
 cv2.destroyWindow('Image')
