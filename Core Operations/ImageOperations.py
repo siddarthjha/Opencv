1f7f59a2ca649d37bea6c1c93fa3a55a3dbfb8fa
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 12:04:52 +0530

    Update ImageOperations.py

--- Core Operations/ImageOperations.py
+++ Core Operations/ImageOperations.py
@@ -4,6 +4,7 @@ import cv2
 img = cv2.imread('a.jpg')
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
 cv2.imshow('Image', img)
+# Pixel values of given row column of the image
 green = img[100, 100, 1]
 red = img[100, 100, 2]
 print(green)
