c1b650f8f29095b5b37545ea3d9f214394bc039b
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 12:11:26 +0530

    Update ImageOperations.py

--- Core Operations/ImageOperations.py
+++ Core Operations/ImageOperations.py
@@ -14,6 +14,10 @@ print(all)
 # You can also change the value
 img[100, 100] = [255, 0, 0]
 print(img[100, 100)
-# Numpy is a optimized library for fast array calculations. So simply accessing each and every pixel values and modifying it will be very slow and it is discouraged.         
+# Numpy is a optimized library for fast array calculations. So simply accessing each and every pixel values and modifying it will be very slow and it is discouraged.  
+# Better pixel accessing and editing method : 
+print(img.item(100, 100, 1))
+img.itemset((10, 10, 2), 250)
+print(img.item(10, 10, 2))          
 cv2.waitKey(0)
 cv2.destroyWindow('Image')
