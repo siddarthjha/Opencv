a3fc2d238240e5dbb694d33c5661ce0035e039c1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 20:02:48 +0530

    Update ArithmeticOperations.py

--- Core Operations/ArithmeticOperations.py
+++ Core Operations/ArithmeticOperations.py
@@ -1,1 +1,12 @@
+cv2.namedWindow('dst', cv2.WINDOW_NORMAL)
+img1 = cv2.imread('a.jpg')
+img2 = cv2.imread('x.jpg')
+print(img1.size)
+print(img2.size)
 
+dst = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+
+cv2.imshow('dst', dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
