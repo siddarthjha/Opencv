c1862211ede3328859512120f6cb6b13a338f710
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 20:04:39 +0530

    Update ArithmeticOperations.py

--- Core Operations/ArithmeticOperations.py
+++ Core Operations/ArithmeticOperations.py
@@ -7,8 +7,10 @@ img1 = cv2.imread('a.jpg')
 img2 = cv2.imread('x.jpg')
 print(img1.size)
 print(img2.size)
+# The size must be same or else it will give you a error
 
 dst = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+# the values are of alpha to display the % of image more
 
 cv2.imshow('dst', dst)
 
