52de0772b2c35d5b36792b62b50286fb01d42faa
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-08 18:12:58 +0530

    Update ImageOperations.py

--- Core Operations/ImageOperations.py
+++ Core Operations/ImageOperations.py
@@ -33,3 +33,34 @@ b,g,r = cv2.split(img)
 img = cv2.merge((b,g,r))
 cv2.waitKey(0)
 cv2.destroyWindow('Image')
+
+# Making Borders for Images (Padding)
+
+BLUE = [255,0,0]
+
+img1 = cv2.imread('a.jpg')
+img1 = img1[:, :, 0]
+
+replicate = cv2.copyMakeBorder(img1,100,100,100,100,cv2.BORDER_REPLICATE)
+reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT_101)
+wrap = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_WRAP)
+constant= cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=BLUE)
+
+plt.subplot(231)
+plt.imshow(img1, 'gray')
+plt.title('ORIGINAL')
+plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')
+plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')
+plt.subplot(234)
+plt.imshow(reflect101,'gray')
+plt.title('REFLECT_101')
+plt.subplot(235)
+plt.imshow(wrap,'gray')
+plt.title('WRAP')
+plt.subplot(236)
+plt.imshow(constant,'gray')
+plt.title('CONSTANT')
+plt.xticks([])
+plt.yticks([])
+plt.show()
