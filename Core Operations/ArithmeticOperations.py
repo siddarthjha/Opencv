61892d162bd2c0bdc478b3ad82b965e92b47f48e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-09 09:56:03 +0530

    Update ArithmeticOperations.py

--- Core Operations/ArithmeticOperations.py
+++ Core Operations/ArithmeticOperations.py
@@ -32,7 +32,7 @@ roi = img1[0:rows, 0:cols ]
 print(roi.shape)
 # Now create a mask of logo and create its inverse mask also
 img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY)
+ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
 cv2.imshow('Mask inverse', mask_inv)
 # Now black-out the area of logo in ROI
@@ -50,15 +50,15 @@ cv2.imshow('res', img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # Now black-out the area of logo in ROI
-img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+img1_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
 
 # Take only region of logo from logo image.
-img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+img2_fg = cv2.bitwise_and(img2, img2, mask = mask)
 
 # Put logo in ROI and modify the main image
-dst = cv2.add(img1_bg,img2_fg)
+dst = cv2.add(img1_bg, img2_fg)
 img1[0:rows, 0:cols ] = dst
 
-cv2.imshow('res',img1)
+cv2.imshow('res', img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
