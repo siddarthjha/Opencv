1c85baf874ce4ecd8c2972488e59fa43377c90e7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-09 09:26:03 +0530

    Update ArithmeticOperations.py

--- Core Operations/ArithmeticOperations.py
+++ Core Operations/ArithmeticOperations.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2 
 
-
-cv2.namedWindow('dst', cv2.WINDOW_NORMAL)
+cv2.namedWindow('add', cv2.WINDOW_NORMAL)
 img1 = cv2.imread('a.jpg')
 img2 = cv2.imread('x.jpg')
 print(img1.size)
@@ -15,4 +14,36 @@ dst = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
 cv2.imshow('dst', dst)
 
 cv2.waitKey(0)
-cv2.destroyWindow('dst')
+cv2.destroyWindow('add')
+
+# BITWISE OPERATIONS
+
+# Load two images
+cv2.namedWindow('res', cv2.WINDOW_NORMAL)
+img1 = cv2.imread('a.jpg')
+img2 = cv2.imread('opencv_logo.png')
+print(img2.shape)
+print(img1.shape)
+
+# I want to put logo on top-left corner, So I create a ROI
+rows,cols,channels = img2.shape
+roi = img1[0:rows, 0:cols ]
+
+# Now create a mask of logo and create its inverse mask also
+img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+# Now black-out the area of logo in ROI
+img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+
+# Take only region of logo from logo image.
+img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+
+# Put logo in ROI and modify the main image
+dst = cv2.add(img1_bg,img2_fg)
+img1[0:rows, 0:cols ] = dst
+
+cv2.imshow('res',img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
