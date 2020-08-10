b83a1becebc2f818f56eff35f78da185152e4219
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-10 19:12:59 +0530

    Update Thresholding.py

--- Image Processing/Thresholding.py
+++ Image Processing/Thresholding.py
@@ -37,11 +37,13 @@ ret,thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) # ADAPTIVE THRESHOLDING
 th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 ret2,th4 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) # OTSU'S BINARIZATION
+blur = cv2.GaussianBlur(img,(5,5),0) # Removal of noise with gaussian for OTSU'S Binarization.
+ret3, th5 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV', 'Adaptive Mean', 'Adaptive Gaussian', 'Otsu']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5, th2, th3, th4]
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV', 'Adaptive Mean', 'Adaptive Gaussian', 'Otsu', 'Gaussian Otsu']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5, th2, th3, th4, th5]
 
-for i in range(9):
+for i in range(10):
     
     plt.subplot(3, 3, i+1)   # Sub-plot (n rows, ncolumns, indexof the particular plot
     plt.imshow(images[i], 'gray') 
