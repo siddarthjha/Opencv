7a8add91bbd8ff160460385271afedf99dcd562e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-10 12:41:04 +0530

    Update Thresholding.py

--- Image Processing/Thresholding.py
+++ Image Processing/Thresholding.py
@@ -7,20 +7,44 @@ OpenCV provides different styles of thresholding and it is decided by the fourth
 """
 # Simple Thresholding
 
-img = cv2.imread('b.jpg')
+# Adaptive Thresholding
+
+"""
+The function used is cv2.threshold. 
+First argument is the source image, which should be a grayscale image.
+Second argument is the  maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value.
+Third argument is the
+ADAPTIVE_THRESH_MEAN_C − threshold value is the mean of neighborhood area. (OR)
+ADAPTIVE_THRESH_GAUSSIAN_C − threshold value is the weighted sum of neighborhood values where weights are a Gaussian window.
+Fourth Argument is the threshold type -> variable of integer type representing the type of threshold to be used.
+Fifth Argument is the blockSize − A variable of the integer type representing size of the pixelneighborhood used to calculate the threshold value.
+Sixth Argument is C (Constant) − A variable of double type representing the constant used in the both methods (subtracted from the mean or weighted mean).
+
+"""
+
+img = cv2.imread('b.jpg', 0)
 ret,thresh1 = cv2.threshold(img, 130, 255, cv2.THRESH_BINARY)
 ret,thresh2 = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY_INV)
 ret,thresh3 = cv2.threshold(img, 140, 255, cv2.THRESH_TRUNC)
 ret,thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
 ret,thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-for i in range(6):
-    images[i] = cv2.cvtColor(images[i], cv2.COLOR_RGB2BGR)
-    plt.subplot(3, 2, i+1),plt.imshow(images[i], 'gray') # Sub-plot (n rows, ncolumns, indexof the particular plot
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV', 'Adaptive Mean', 'Adaptive Gaussian']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5, th2, th3]
+
+for i in range(8):
+    
+    plt.subplot(3, 3, i+1)   # Sub-plot (n rows, ncolumns, indexof the particular plot
+    plt.imshow(images[i], 'gray') 
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
 plt.show()
+
+cv2.imshow('Mean', th2)
+cv2.imshow('Gaussian', th3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
