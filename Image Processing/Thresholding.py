ce69b692ee3e0e10c9affb21cb41e2c4d81cd3bb
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-10 10:24:41 +0530

    Update Thresholding.py

--- Image Processing/Thresholding.py
+++ Image Processing/Thresholding.py
@@ -1,1 +1,7 @@
-
+"""
+The function used is cv2.threshold. 
+First argument is the source image, which should be a grayscale image.
+Second argument is the threshold value which is used to classify the pixel values. 
+Third argument is the maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value.
+OpenCV provides different styles of thresholding and it is decided by the fourth parameter of the function. 
+"""
