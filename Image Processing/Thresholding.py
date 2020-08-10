e3fc4df73bf0bfcb1c03343df98fadc635c24326
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-10 19:16:29 +0530

    Update Thresholding.py

--- Image Processing/Thresholding.py
+++ Image Processing/Thresholding.py
@@ -1,7 +1,7 @@
 """
 The function used is cv2.threshold. 
 First argument is the source image, which should be a grayscale image.
-Second argument is the threshold value which is used to classify the pixel values. 
+Second argument is the threshold value which is used to classify the pixel values. (For Otsu Binarization pass 0)
 Third argument is the maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value.
 OpenCV provides different styles of thresholding and it is decided by the fourth parameter of the function. 
 """
@@ -36,8 +36,8 @@ ret,thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
 th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) # ADAPTIVE THRESHOLDING
 th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-ret2,th4 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) # OTSU'S BINARIZATION
-blur = cv2.GaussianBlur(img,(5,5),0) # Removal of noise with gaussian for OTSU'S Binarization.
+ret2, th4 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU) # OTSU'S BINARIZATION
+blur = cv2.GaussianBlur(img, (5, 5), 0) # Removal of noise with gaussian for OTSU'S Binarization.
 ret3, th5 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV', 'Adaptive Mean', 'Adaptive Gaussian', 'Otsu', 'Gaussian Otsu']
@@ -45,10 +45,10 @@ images = [img, thresh1, thresh2, thresh3, thresh4, thresh5, th2, th3, th4, th5]
 
 for i in range(10):
     
-    plt.subplot(3, 3, i+1)   # Sub-plot (n rows, ncolumns, indexof the particular plot
+    plt.subplot(4, 3, i+1)   # Sub-plot (n rows, ncolumns, indexof the particular plot
     plt.imshow(images[i], 'gray') 
     plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
+    plt.xticks([]), plt.yticks([])
 
 plt.show()
 
