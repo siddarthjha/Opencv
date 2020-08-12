470e441761a6ad7ee8108c7dce326d65cb597d7e
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-12 18:19:36 +0530

    Update Geometric_Transformations.py

--- Image Processing/Geometric_Transformations.py
+++ Image Processing/Geometric_Transformations.py
@@ -58,3 +58,19 @@ plt.subplot(122)
 plt.imshow(dst)
 plt.title('Output')
 plt.show()
+
+# Perspective Transformation
+img = cv2.imread('b.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+rows, cols, ch = img.shape
+
+pts1 = np.float32([[56, 65], [368, 52], [28, 387], [389, 390]])
+pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
+
+M = cv2.getPerspectiveTransform(pts1, pts2)
+
+dst = cv2.warpPerspective(img, M, (300, 300))
+
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
