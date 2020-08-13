b91447cb0943b7d7fc617c98c90894fb58f00bad
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-13 19:39:35 +0530

    Update Filters.py

--- Image Processing/Filters.py
+++ Image Processing/Filters.py
@@ -13,29 +13,37 @@ plt.xticks([]), plt.yticks([])
 plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
 plt.xticks([]), plt.yticks([])
 plt.show()
+
 # Averaging
 img = cv2.imread('b.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 blur = cv2.blur(img, (5, 5))
+
 # Gaussian Blur
 gblur = cv2.GaussianBlur(img, (7,7), 0)
+
+# Median Blur
 img1 = cv2.imread('n.jpg')
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-# Median Blur
+
 # Take a noisy image for this
 median = cv2.medianBlur(img1, 5)
+
+# Bilateral Filtering
 bblur = cv2.bilateralFilter(img,9,75,75)
+
+# Plotting all of them
 plt.subplot(321), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(322), plt.imshow(blur), plt.title('Blurred')
+plt.subplot(322), plt.imshow(blur), plt.title('Averaging(Blurred)')
 plt.xticks([]), plt.yticks([])
-plt.subplot(323), plt.imshow(gblur), plt.title('Gaussian Blurred')
+plt.subplot(323), plt.imshow(gblur), plt.title('Gaussian Filtering')
 plt.xticks([]), plt.yticks([])
 plt.subplot(324), plt.imshow(img1), plt.title('Median Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(325), plt.imshow(median), plt.title('Median Blurred')
+plt.subplot(325), plt.imshow(median), plt.title('Median Filtering')
 plt.xticks([]), plt.yticks([])
-plt.subplot(326), plt.imshow(bblur), plt.title('Bilateral Filter') # Note surface texture is gone but edges are still preservec
+plt.subplot(326), plt.imshow(bblur), plt.title('Bilateral Filtering') # Note surface texture is gone but edges are still preservec
 plt.xticks([]), plt.yticks([])
 
 plt.show()
