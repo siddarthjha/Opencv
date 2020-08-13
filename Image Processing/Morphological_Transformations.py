a4f54a3708dc41184f6b38774a74c591f835a6dc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-13 22:09:17 +0530

    Update Morphological_Transformations.py

--- Image Processing/Morphological_Transformations.py
+++ Image Processing/Morphological_Transformations.py
@@ -3,10 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('b.jpg', 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 kernel = np.ones((5, 5), np.uint8)
+
+# Erosion - It is useful for removing small white noises
 erosion = cv2.erode(img, kernel, iterations = 1)
 
-plt.subplot(121), plt.imshow(img), plt.title('Original')
+# dilation - 
+dilation = cv2.dilate(img,kernel,iterations = 1)
+# erosion removes white noises, but it also shrinks our object.
+# So we dilate it. Since noise is gone, they won’t come back, but our object area increases.
+
+# Plotting
+plt.subplot(221), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(erosion), plt.title('Erosion')
+plt.subplot(222), plt.imshow(erosion), plt.title('Erosion')
 plt.xticks([]), plt.yticks([])
+plt.subplot(223), plt.imshow(dilation), plt.title('Dilation')
+plt.xticks([]), plt.yticks([])
+plt.show()
