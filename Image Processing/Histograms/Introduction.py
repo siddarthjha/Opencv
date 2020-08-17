1fea7ba3f6ddd6876e23fa57f7ace11f1af72ef2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-17 19:28:09 +0530

    Update Introduction.py

--- Image Processing/Histograms/Introduction.py
+++ Image Processing/Histograms/Introduction.py
@@ -14,3 +14,11 @@ plt.show()
 # hist, bins = np.histogram(img.ravel(), 256, [0, 256])
 plt.hist(img.ravel(), 256, [0, 256])
 plt.show()
+
+# Another way of matplotlib of BGR
+color = ('b','g','r')
+for i,col in enumerate(color):
+    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+plt.show()
