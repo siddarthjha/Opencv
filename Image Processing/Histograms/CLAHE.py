b4c427fc9dc8bcade1554724612923ed3d2e4da2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-18 18:17:08 +0530

    Update CLAHE.py

--- Image Processing/Histograms/CLAHE.py
+++ Image Processing/Histograms/CLAHE.py
@@ -13,4 +13,20 @@ cv2.imshow('quant', cl1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# CLAHE for Color images
 
+bgr = cv2.imread('b.jpg')
+lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+# L-lightness, a & b for color components green-red & blue-yellow
+
+
+clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(10, 10))
+
+lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+
+qnt = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR) 
+hs = np.hstack((bgr, qnt))
+cv2.namedWindow('quant', cv2.WINDOW_NORMAL)
+cv2.imshow('quant', hs)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
