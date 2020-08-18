26718d8cde95be08c2896c9238b31964b44507ff
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-18 17:54:09 +0530

    Update CLAHE.py

--- Image Processing/Histograms/CLAHE.py
+++ Image Processing/Histograms/CLAHE.py
@@ -1,1 +1,16 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('a.jpg',0)
+
+# create a CLAHE object (Arguments are optional).
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) # The lower the clip-limit the better
+# Clip-limit is the contrast of image (1-3)
+cl1 = clahe.apply(img)
+cl1 = np.hstack((img, cl1))
+cv2.namedWindow('quant', cv2.WINDOW_NORMAL)
+cv2.imshow('quant', cl1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
