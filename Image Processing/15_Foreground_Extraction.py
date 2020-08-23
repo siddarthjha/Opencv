d3cb1669d2d08400fe8c0ba6d182061621d76611
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-23 21:26:26 +0530

    Update 15_Foreground_Extraction.py

--- Image Processing/15_Foreground_Extraction.py
+++ Image Processing/15_Foreground_Extraction.py
@@ -3,4 +3,16 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('b.jpg')
-mask = np.zeros(img.shape[:2],np.uint8)
+print(img.shape[:2])
+mask = np.zeros(img.shape[:2], np.uint8)
+
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
+
+rect = (180, 30, 500, 1000)
+cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 8, cv2.GC_INIT_WITH_RECT)
+
+mask2 = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8')
+img = img * mask2[:, :, np.newaxis]
+
+plt.imshow(img), plt.colorbar(), plt.show()
