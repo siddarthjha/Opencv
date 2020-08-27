ccfddfc063ab69df8283ac3a6ba42f0ffae9b508
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-27 22:01:59 +0530

    Update 01_HarrisDetection.py

--- Feature Detection/01_HarrisDetection.py
+++ Feature Detection/01_HarrisDetection.py
@@ -4,22 +4,18 @@ import cv2
 img = cv2.imread('b.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+
+# find Harris corners
 gray = np.float32(gray)
+
+# img should be grayscale and float type, blocksize size of neighbourhood, ksize aperture parameter of sobel derivative used, 
+# k harris detector free parameter in the equation
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-# img- should be grayscale and float type, blocksize- size of neighbourhood, ksize- aperture parameter of sobel derivative used, 
-# k- harris detector free parameter in the equation
+
 
 #result is dilated for marking the corners, not important
 dst = cv2.dilate(dst, None)
 
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0, 0, 255]
-
-cv2.imshow('dst',img)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
-
-# Corner with Sub-pixel accuracy
 ret, dst = cv2.threshold(dst, 0.01*dst.max(), 255, 0)
 dst = np.uint8(dst)
 
@@ -33,7 +29,9 @@ corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criter
 # Now draw them
 res = np.hstack((centroids, corners))
 res = np.int0(res)
-img[res[:, 1], res[:,0]]=[0, 0, 255]
-img[res[:, 3], res[:,2]] = [0, 255, 0]
+img[res[:, 1], res[:, 0]]=[0, 0, 255]
+img[res[:, 3], res[:, 2]] = [0, 255, 0]
 
-cv2.imwrite('subpixel5.png', img)    
+cv2.imshow('harris detection', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
