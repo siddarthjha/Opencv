30581d623e762bf6e119742c4c6abc89f5d1477a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-02 18:33:14 +0530

    Update 03_SIFT.py

--- Feature Detection/03_SIFT.py
+++ Feature Detection/03_SIFT.py
@@ -1,12 +1,27 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('home.jpg')
-gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# INSTALL opencv-contrib-python
+# As SIFT and SURF is removed from opencv
 
-sift = cv2.SIFT()
-kp = sift.detect(gray, None)
+img = cv2.imread('b.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img=cv2.drawKeypoints(gray, kp)
 
-cv2.imwrite('sift_keypoints.jpg', img)
+sift = cv2.xfeatures2d.SIFT_create()
+kps = sift.detect(gray, None)
+
+kp,des = sift.compute(gray, kps) # Computes Keypoint descriptor from the computed keypoints
+print(des)
+
+img = cv2.drawKeypoints(gray, kps, outImage = None)
+
+cv2.imshow('vgf', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# The flag will draw a circle with size of keypoint and show it's orientation 
+img = cv2.drawKeypoints(gray, kps, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, outImage = None)
+cv2.imshow('vgf', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
