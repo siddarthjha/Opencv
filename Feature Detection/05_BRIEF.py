06d7096edc8a27015b6f8b4048fda2e043a31adc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-03 13:47:01 +0530

    Update 05_BRIEF.py

--- Feature Detection/05_BRIEF.py
+++ Feature Detection/05_BRIEF.py
@@ -3,18 +3,22 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('b.jpg',0)
+print(img.shape)
 
 # Initiate STAR detector
-star = cv2.FeatureDetector_create("STAR")
+star = cv2.xfeatures2d.StarDetector_create()
 
 # Initiate BRIEF extractor
-brief = cv2.DescriptorExtractor_create("BRIEF")
+brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
 
 # find the keypoints with STAR
 kp = star.detect(img,None)
 
 # compute the descriptors with BRIEF
 kp, des = brief.compute(img, kp)
-
-print(brief.getInt('bytes'))
+img = cv2.drawKeypoints(img, kp, outImage = None)
+cv2.imshow('BRIEF', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(brief.getByte())
 print(des.shape)
