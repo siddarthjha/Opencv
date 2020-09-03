9831e74dd99a3edab4bb21911b8ffd80a21c9d0c
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-03 13:46:36 +0530

    Update 04_FAST.py

--- Feature Detection/04_FAST.py
+++ Feature Detection/04_FAST.py
@@ -6,11 +6,11 @@ img = cv2.imread('b.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Initiate FAST object with default values
-fast = cv2.FastFeatureDetector(40)
+fast = cv2.FastFeatureDetector_create(40)
 
 # find and draw the keypoints
 kp = fast.detect(img,None)
-img2 = cv2.drawKeypoints(img, kp, color=(255,0,0))
+img2 = cv2.drawKeypoints(img, kp, color=(255,0,0), outImage = None)
 cv2.imshow('Fast true', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -18,9 +18,9 @@ cv2.imwrite('fast_true.png',img2)
 
 
 # Print all default params
-print("Threshold: ", fast.getInt('threshold'))
-print("nonmaxSuppression: ", fast.getBool('nonmaxSuppression'))
-print("neighborhood: ", fast.getInt('type'))
+print("Threshold: ", fast.getThreshold)
+print("nonmaxSuppression: ", fast.getNonmaxSuppression())
+print("neighborhood: ", fast.getType())
 print("Total Keypoints with nonmaxSuppression: ", len(kp))
 
 cv2.imshow('Fast true', img2)
@@ -28,14 +28,13 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Disable nonmaxSuppression
-fast.setBool('nonmaxSuppression',0)
+fast.setNonmaxSuppression(False)
 kp = fast.detect(img,None)
 
 print("Total Keypoints without nonmaxSuppression: ", len(kp))
 
-img3 = cv2.drawKeypoints(img, kp, color=(255,0,0))
+img3 = cv2.drawKeypoints(img, kp, color=(255,0,0), outImage = None)
 
 cv2.imshow('fast_false', img3)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
