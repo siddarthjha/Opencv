0728415189afe59f6b048ae4570263efaa26e0bf
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-09-04 21:21:03 +0530

    Update 07_FeatureMatching.py

--- Feature Detection/07_FeatureMatching.py
+++ Feature Detection/07_FeatureMatching.py
@@ -1,1 +1,49 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
+img1 = cv2.imread('b_.jpg', 0)          # queryImage
+img2 = cv2.imread('b.jpg', 0) # trainImage
+
+# Initiate ORB detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1, des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], flags=2, outImg = None)
+
+plt.imshow(img3),plt.show()
+
+# SIFT descriptor
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift.detectAndCompute(img2, None)
+
+# BFMatcher with default params
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+
+# Apply ratio test
+good = []
+for m, n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+
+# cv2.drawMatchesKnn expects list of lists as matches.
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, flags=2, outImg = None)
+
+plt.imshow(img3), plt.show()
